e1890e08d40eb7ea8958957ebdb50f75048792e9
ProjectDoodle <boba1464@gmail.com>
2023-07-30 12:01:02 -0700

    solved

--- 2021/tunnel-vision/parse-header.py
+++ 2021/tunnel-vision/parse-header.py
@@ -1,5 +1,5 @@
 # Opens file in read mode as bytes
-file = open("tunn3l_v1s10n", "rb")
+file = open("tunn3l_v1s10n_corrected_v2.bmp", "rb")
 fbytes = file.read()
 
 # Bitmap File Header (14 bytes, not optional)
@@ -25,6 +25,11 @@ bmpHeight = int.from_bytes(fbytes[22:26], byteorder="little")
 colorPlanes = int.from_bytes(fbytes[26:28], byteorder="little")
 bitsPerPixel = int.from_bytes(fbytes[28:30], byteorder="little")
 compressionMethod = fbytes[30:34]
+imageSize = int.from_bytes(fbytes[34:38], byteorder="little")
+horRes = int.from_bytes(fbytes[38:42], byteorder="little")
+verRes = int.from_bytes(fbytes[42:46], byteorder="little")
+numColors = int.from_bytes(fbytes[46:50], byteorder="little")
+numImportantColors = int.from_bytes(fbytes[50:54], byteorder="little")
 
 print("DIB Size: " + str(dibSize))
 print("DIB Size bytes: {}".format(fbytes[14:18]))
@@ -33,6 +38,11 @@ print("BM Height: " + str(bmpHeight))
 print("Color Planes: " + str(colorPlanes))
 print("Bits Per Pixel: " + str(bitsPerPixel))
 print("Compression Method: " + str(compressionMethod))
+print("Image Size: " + str(imageSize))
+print("Horizontal Resolution: " + str(horRes))
+print("Vertical Resolution: " + str(verRes))
+print("Number of Colors: " + str(numColors))
+print("Number of Important Colors: " + str(numImportantColors))
 
 # No "Extra Bit Masks"
 
